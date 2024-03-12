b26c9c27b44a090235976bf0dbd74d88f2fc00b1
Lavakumaras <lavakumaras.93@gmail.com>
2024-03-12 17:43:08 +0530

    new class added

---
+++ src/test/java/practiceMaven/newclassTest.java
@@ -0,0 +1,40 @@
+package practiceMaven;
+
+import org.testng.annotations.Test;
+
+public class newclassTest {
+@Test(groups= "smoke")
+public void script1Test()
+{
+	System.out.println("changes for conflict");
+	}
+
+@Test
+public void script2Test()
+{
+	System.out.println("--test Sript2");
+	}
+
+
+@Test
+public void script03Test()
+{
+	System.out.println("--test Sript03");
+	}
+@Test
+public void script04Test()
+{
+	System.out.println("--test Sript04");
+	}
+
+@Test
+public void script05Test()
+{
+	System.out.println("--test Sript05");
+	}
+@Test
+public void script06Test()
+{
+	System.out.println("--test Sript06");
+	}
+}
