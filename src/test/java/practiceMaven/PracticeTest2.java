fe993fe27f67aa347b17cc52bf3fedff8cb07bfb
Lavakumaras <lavakumaras.93@gmail.com>
2024-03-12 15:46:38 +0530

    new class added

---
+++ src/test/java/practiceMaven/PracticeTest2.java
@@ -0,0 +1,29 @@
+package practiceMaven;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest2 {
+	@Test(groups= "smoke")
+	public void script5Test()
+	{
+		System.out.println("--test Sript5");
+		}
+
+	@Test(groups= {"smoke","regression"})
+	public void script6Test()
+	{
+		System.out.println("--test Sript6");
+		}
+	
+	@Test(groups= "smoke")
+	public void script06Test()
+	{
+		System.out.println("--test Sript06");
+		}
+
+	@Test(groups= {"smoke","regression"})
+	public void script07Test()
+	{
+		System.out.println("--test Sript07");
+		}
+}
